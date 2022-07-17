fd654b0b38aff3ef13302c0b26a0797c11b16455
Aibek <aibek.almabekuulu@mail.ru>
2022-07-17 19:52:41 +0600

    update: update  solution in index.js

--- src/index.js
+++ src/index.js
@@ -35,11 +35,22 @@ const MORSE_TABLE = {
     '---..':  '8',
     '----.':  '9',
     '-----':  '0',
+    '**********': ' ',
 };
+const expr = '00101010100000000010001011101000101110100000111111**********00001011110000111111000010111000101110100000111010';
 
-function decode(expr) {
-    // write your solution here
+function decode(expr){
+    let str=""; 
+    let symbols=expr;
+    for(let i=0;i<expr.length;i+=10){
+        symbols=expr.slice(i,i+10);
+          while(symbols.includes("10")||symbols.includes("11")||symbols.includes("00")){     
+          symbols=symbols.replace("10",".").replace("11","-").replace("00","");}
+        str+=symbols.includes("**********")?" ":MORSE_TABLE[symbols];
+    }
+   return str;
 }
+
 
 module.exports = {
     decode
